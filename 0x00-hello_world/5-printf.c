01853a9252a9813451df3a1b98d4b8e9765e5a4b
ezekielmacla <ezekielmacla056@gmail.com>
2022-09-11 23:16:43 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - print the script in the put function
+*
+* Descprition: using the main function
+* this program prints "programming is like building a multilingual puzzle
+* Return
+*/
+int main(void)
+{
+       printf("with proper grammar, but the outcome is a piece of art,\n");
+       Return (0);
+}
