80248cb5d7aad8a88ab171dd16b2f69f0921c068
ezekielmacla <ezekielmacla056@gmail.com>
2022-09-22 01:57:47 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,3 @@
-#include "main.h"                                                  
-                                                                   
 /**                                                                
 *reset_to_98 - resets the value of given integer to 98             
 *@n: parameter                                                     
