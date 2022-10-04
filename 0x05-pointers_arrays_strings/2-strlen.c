f2ffbb5a2b63ed5b5b2268a1f8cd8111c1d0d97d
ezekielmacla <ezekielmacla056@gmail.com>
2022-10-04 04:14:55 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,27 +1,16 @@
 #include "main.h"
 
 /**
- * _strncpy - cpoies n bytes of src to the dest string
- * @dest: string to copy to
- * @src: string being copied
- * @n: largest number of bytes to copy
+ * _strlen - returns the length of a string
+ * @s: string to find length of
  *
- * Return: address of dest
+ * Return: length of s
  */
-char *_strncpy(char *dest, char *src, int n)
+int _strlen(char *s)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
-	while (i < n && *(src + i))
-	{
-		*(dest + i) = *(src + i);
+	while (*(s + i))
 		i++;
-	}
-	while (i < n)
-	{
-		*(dest + i) = '\0';
-		i++;
-	}
-	return (dest);
+	return (i);
 }
